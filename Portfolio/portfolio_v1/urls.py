f9f93921f9558e0c8407bdbcd166cf652123c5f9
bharat chitara <bharatchitara99@gmail.com>
2022-07-17 18:51:16 +0530

    home page created

--- Portfolio/portfolio_v1/urls.py
+++ Portfolio/portfolio_v1/urls.py
@@ -2,8 +2,10 @@ from django.urls import path, re_path
 
 from portfolio_v1 import views
 
+
 urlpatterns = [ 
-            path("",views.base,name = "base" )
-               
+            path("",views.base,name = "index"),
+            path("about",views.about,name= "about"),
+        
                ]
 
--- Portfolio/portfolio_v1/views.py
+++ Portfolio/portfolio_v1/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 
 def base(request):
-    return render(request, 'base.html')
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request,'about.html')
+
      
+     
+### for testing
+
+def test(request):
+    return render (request, 'base.html')     
